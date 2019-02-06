2ae30a8f2f77334e85fa84a3e257064a143cb68f
Chinyiu Chau <>
2019-02-06 23:03:09 +0100

    whatup

--- Sources/App/Controllers/WeatherController.swift
+++ Sources/App/Controllers/WeatherController.swift
@@ -6,35 +6,77 @@
 //
 
 import Vapor
+import Foundation
 
 class WeatherController {
     
-    
+    typealias WeatherRequestCompletionHandler = (_ result: WeatherResponse?, _ error: Error?)-> Void
+    typealias ImageRequestCompletionHandler = (_ result:ImageResponse?, _ error: Error?)-> Void
+
     func city(_ req: Request, name: String) throws -> Future<Response> {
+        
+        
         let client = try req.make(Client.self)
         
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        self.requestImage(client, name) { (imageResponse, error) in
+            guard let image = imageResponse, error == nil else {
+                 dispatchGroup.leave()
+                return
+            }
+            print(image)
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        self.requestWeather(client, name) { (weatherResponse, error) in
+            guard let weather = weatherResponse, error == nil else {
+                dispatchGroup.leave()
+                return
+            }
+            print(weather)
+            dispatchGroup.leave()
+        }
+        
+       dispatchGroup.wait()
    
+       print("Both functions complete 👍")
         let response = client.get("https://api.openweathermap.org/data/2.5/weather?q=\(name)&appid=b50a8ebaff3e45413c6fe674489f5541&units=metric")
-       
+            
+       return response
+ 
+    }
+    
+    func requestWeather(_ client: Client,_ name: String, completionHandler: @escaping WeatherRequestCompletionHandler ){
+        let response = client.get("https://api.openweathermap.org/data/2.5/weather?q=\(name)&appid=b50a8ebaff3e45413c6fe674489f5541&units=metric")
+        
         let weatherResponse:Future<WeatherResponse> = response.flatMap{try $0.content.decode(WeatherResponse.self)}
         
         weatherResponse.do { weather in
-            guard let detail = weather.weather.first else {
-               return
-            }
-            
-            print(detail.main + "," + detail.description)
-        
-         }.catch { error in
-                    print(error) // A Swift Error
-          }
-        
-        
-       let imagesResponse = client.get("https://api.unsplash.com/search/photos?page=1&query=\(name)&client_id=284ab250f4d0cd5fe2d1538ffa5d30a58d76e96fadfc90362d0a7aeb3191ef20")
- 
-        return response
-
+             completionHandler(weather,nil)
+            }.catch { error in
+                print(error) // A Swift Error
+              completionHandler(nil,error)
+        }
     }
+
     
- 
+    func requestImage(_ client: Client, _ name: String, completionHandler: @escaping ImageRequestCompletionHandler ){
+        let response = client.get("https://api.unsplash.com/search/photos?page=1&query=\(name)&client_id=284ab250f4d0cd5fe2d1538ffa5d30a58d76e96fadfc90362d0a7aeb3191ef20")
+        
+        
+        let imageResponse = response.flatMap{try $0.content.decode(ImageResponse.self)}
+        
+        imageResponse.do { image in
+            completionHandler(image,nil)
+            }.catch { error in
+                print(error) // A Swift Error
+                completionHandler(nil,error)
+        }
+    }
+
 }
+
+
--- Sources/App/Models/ImageResponse.swift
+++ Sources/App/Models/ImageResponse.swift
@@ -8,6 +8,20 @@
 import Vapor
 
 struct ImageResponse: Content {
-    
+    var results: [Image]
 
+}
+
+struct Image : Content {
+    var id: String
+    var urls: Url
+    var description: String
+}
+
+struct Url: Content {
+    var raw: String
+    var full: String
+    var regular: String
+    var small: String
+    var thumb: String
 }
